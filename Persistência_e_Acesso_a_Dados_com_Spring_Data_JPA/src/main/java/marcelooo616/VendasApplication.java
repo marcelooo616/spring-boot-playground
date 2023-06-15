27de1d642c9feef4b37e15f9cfe63738ffc7846f
marcelooo616 <marcelooo2014@hotmail.com>
2023-06-15 19:59:26 -0300

    feat -Salvando e recuperando clientes

--- Persistência_e_Acesso_a_Dados_com_Spring_Data_JPA/src/main/java/marcelooo616/VendasApplication.java
+++ Persistência_e_Acesso_a_Dados_com_Spring_Data_JPA/src/main/java/marcelooo616/VendasApplication.java
@@ -2,13 +2,33 @@ package marcelooo616;
 
 
 
+import marcelooo616.domain.entity.Cliente;
+import marcelooo616.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return  args -> {
+            clientes.salvar(new Cliente("Marcelo"));
+            clientes.salvar(new Cliente("Pedro"));
+            List<Cliente> getAll = clientes.obterTodos();
+            getAll.forEach(System.out::println);
+
+        };
+    }
+
+
 
 
     public static void main(String[] args) {
