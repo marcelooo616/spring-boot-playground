9ebbb7d7dcac8d6ba43f88e612aee35119eb7b67
marcelooo616 <marcelooo2014@hotmail.com>
2023-06-15 21:21:11 -0300

    projeto d teste

--- Persistência_e_Acesso_a_Dados_com_Spring_Data_JPA/src/main/java/marcelooo616/domain/repositorio/Clientes.java
+++ Persistência_e_Acesso_a_Dados_com_Spring_Data_JPA/src/main/java/marcelooo616/domain/repositorio/Clientes.java
@@ -16,6 +16,8 @@ public class Clientes {
 
     private static String INSERT = "insert into cliente (nome) values (?)";
     private static String SELECT_ALL = "SELECT * FROM CLIENTE";
+    private static String UPDATE = "update cliente set nome = ? whare id = ? ";
+    private static String DELETE = "delete from cliente whare id = ?" ;
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -25,14 +27,36 @@ public class Clientes {
         return  cliente;
     }
 
+    public Cliente atualizar(Cliente cliente){
+        jdbcTemplate.update(UPDATE, new Object[]{
+                cliente.getNome(), cliente.getId()} );
+        return cliente;
+    }
+
+    public void deletar(Integer id){
+        jdbcTemplate.update(DELETE, new Object[]{id});
+    }
+
+    public List<Cliente> obterProNome(String nome){
+        return jdbcTemplate.query(
+                SELECT_ALL.concat("where nome like ? "),
+                new Object[]{"%" + nome + "%"},
+                obterClienteMapper()
+                );
+    }
+
     public List<Cliente> obterTodos(){
-        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>(){
+        return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
+    }
+
+    private RowMapper<Cliente> obterClienteMapper() {
+        return new RowMapper<Cliente>(){
             @Override
             public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
                 Integer id = resultSet.getInt("id");
                 String nome = resultSet.getString("nome");
                 return new Cliente(id,nome);
             }
-        });
+        };
     }
 }
